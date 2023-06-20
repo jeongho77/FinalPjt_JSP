dfb7acd2bf29670212603bdd0dff6636cb12d0fa
sorae <115053276+Sorae1118@users.noreply.github.com>
2023-06-20 17:42:50 +0900

    Add files via upload

---
+++ Pjt/src/main/webapp/project/login_page/login.js
@@ -0,0 +1,23 @@
+//네비게이션 버튼 클릭시 화면 이동
+function moveMain() {
+    location.href = "../index.html"
+}
+function moveCommunity() {
+    location.href = "../board_page/community.html"
+}
+
+function moveKnowledge() {
+    location.href = "../board_page/knowledge.html"
+}
+
+function moveQ() {
+    location.href = "../board_page/qna.html"
+}
+;
+function moveLogin() {
+    location.href = "../login_page/login.html"
+}
+
+function moveSignup() {
+    location.href = "../login_page/signup.html"
+}
